ce3654d095a654c38d6e59a9a3a906720126e0d9
silvajeanderson165 <silvajeanderson165@gmail.com>
2026-04-23 12:48:48 -0300

    feat: transforma app web padrao em um progressive web app (PWA)

---
+++ sw.js
@@ -0,0 +1,64 @@
+const CACHE_NAME = 'floresta-cache-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './style.css',
+  './engine.js',
+  './entities.js',
+  './game.js',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png',
+  './favicon.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  // Ignora requisições de outras origens ou que não sejam GET
+  if (event.request.method !== 'GET') return;
+  
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request).then(
+          function(response) {
+            if(!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+            var responseToCache = response.clone();
+            caches.open(CACHE_NAME)
+              .then(function(cache) {
+                cache.put(event.request, responseToCache);
+              });
+            return response;
+          }
+        );
+      })
+  );
+});
+
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
